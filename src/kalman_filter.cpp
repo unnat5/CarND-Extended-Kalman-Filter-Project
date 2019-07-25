5042f20ceea3e3a6228e98cb59cca3fe4cce3115
unnat5 <unnatsingh5@gmail.com>
2019-07-25 11:28:17 +0000

    Emplementation of KalmanFilter and EKF update and predict step with measurement sensor dataset.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+
 /* 
  * Please note that the Eigen library does not initialize 
  *   VectorXd or MatrixXd objects with zeros upon creation.
@@ -26,16 +27,61 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_*P_*Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+    VectorXd z_pred = H_*x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd Pht = P_ * Ht;
+    MatrixXd K = Pht * Si;
+    
+    // new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+    VectorXd z_pred = VectorXd(3);
+  	double px,py,vx,vy;
+	px = x_[0],py=x_[1],vx = x_[2], vy = x_[3];
+//   cout<<"yo"<<x_[0]<<"ss"<<x_(0)<<endl;
+  	double a = sqrt(px*px + py*py);
+    double b = atan2(py,px);
+  	double c = (px*vx + py*vy)/a;
+  	z_pred << a,b,c;
+  
+  
+  
+  
+    VectorXd y = z - z_pred;
+  // y(1) normalization https://discussions.udacity.com/t/rmse-values-of-ekf-project/243997
+    while (y(1) > M_PI)
+        y(1) -= 2 * M_PI;
+    while (y(1) < -M_PI)
+        y(1) += 2 * M_PI;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd Pht = P_ * Ht;
+    MatrixXd K = Pht * Si;
+    
+    // new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_)*P_;
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -2,6 +2,9 @@
 #define KALMAN_FILTER_H_
 
 #include "Eigen/Dense"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 class KalmanFilter {
  public:
