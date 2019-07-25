113e0d4010f8c3ae861c75412c37279e0cd0829a
unnat5 <unnatsingh5@gmail.com>
2019-07-25 11:25:10 +0000

    preprocess the measurements \n check the measurement sent by the sensors whether they are of radar or Lidar and if Radar then convert them into polar representation. and if radar then use Kalman filter's predict and update step otherwise EKF's predict and update steps.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Eigen/Dense"
 #include "tools.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -36,6 +37,19 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
+  H_laser_ << 1,0,0,0,
+             0,1,0,0;
+
+  
+  
+
+  
+    // The initial transition matrix F_
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 
 
 }
@@ -60,18 +74,47 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
+    
+      // State covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
+      double rho = measurement_pack.raw_measurements_[0];
+      double phi_measured = measurement_pack.raw_measurements_[1];
+      double rho_dot = measurement_pack.raw_measurements_[2];
+      double x =  rho*cos(phi_measured);
+//       if (x<0.0001){
+//         x = 0.0001;
+//       }
+      double y = rho*sin(phi_measured);
+//       if (y<0.0001){
+//         y = 0.0001;
+//       }
+      ekf_.x_ << x,
+      			y,
+      			rho_dot * cos(phi_measured),
+      			rho_dot * sin(phi_measured);
+//       ekf_.init()
+      
 
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
+      ekf_.x_ << measurement_pack.raw_measurements_[0],
+                  measurement_pack.raw_measurements_[1],
+                  0,
+                  0;
 
     }
 
     // done initializing, no need to predict or update
+     previous_timestamp_ = measurement_pack.timestamp_;
     is_initialized_ = true;
     return;
   }
@@ -86,8 +129,36 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * TODO: Update the process noise covariance matrix.
    * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+   // the initial transitional matrix F_
 
-  ekf_.Predict();
+  
+  // set the acceleration noise components
+  float noise_ax = 9.0;
+  float noise_ay = 9.0;
+  
+  
+
+  
+  
+  	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+    previous_timestamp_ = measurement_pack.timestamp_;
+	ekf_.F_(0,2) = dt;
+    ekf_.F_(1,3) = dt;
+  	 
+  MatrixXd G(4,2);
+      G << (dt*dt)/2, 0,
+            0, (dt*dt)/2,
+            dt,0,
+            0, dt;
+    
+    MatrixXd Q_v(2,2);
+    Q_v<< noise_ax, 0,
+          0, noise_ay;
+    MatrixXd Q_;
+    Q_  =  G*Q_v*G.transpose();
+  	ekf_.Q_ = Q_;
+
+ ekf_.Predict();
 
   /**
    * Update
@@ -101,9 +172,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
+//     Tools tool;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
+    /*
+      x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+    */
+    // so P_ I will get from predict method I feel main purpose of this is to distinguish between Hj and H.
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+
 
   }
 
